fed13e832180890d6f4833babd15c7df5c89e6a2
Michael T. DeFelice <michael.defelice@gmail.com>
2019-06-20 22:06:24 -0500

    JSON-LD

--- gen.sh
+++ gen.sh
@@ -10,6 +10,26 @@ cat << EOF > $OUT
 <meta charset = 'utf-8'>
 <meta name = 'viewport' content = 'width=device-width, initial-scale=1.0'>
 <meta name = 'description' content = 'Resume for Michael DeFelice - a Data Scientist, Developer, and Infrastructure Architect with 15 years of experience and an expertise in machine learning & AI.'>
+<script type = 'application/ld+json'>
+{
+  "@context": "https://schema.org",
+  "@type": "Person",
+  "address": {
+    "@type": "PostalAddress",
+    "addressLocality": "San Antonio",
+    "addressRegion": "TX",
+    "postalCode": "78205"
+  },
+  "jobTitle": "Co-Founder & CTO, CyberFortress",
+  "name": "Michael T. DeFelice",
+  "alumniOf": "Ursinus College",
+  "url": "https://mtdefelice.github.io",
+  "sameAs" : [
+    "https://www.linkedin.com/in/mtdefelice",
+    "https://twitter.com/mtdefelice"
+  ]
+}
+</script>
 <title>Michael DeFelice</title>
 </head>
 <body>
